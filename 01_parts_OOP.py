80024041c82c9300b90069d3af940d58920f37a7
David Guerrero <daviguer890@gmail.com>
2024-05-10 15:12:09 -0500

    Explanation of the destructor method and del statement.

--- 01_parts_OOP.py
+++ 01_parts_OOP.py
@@ -17,21 +17,33 @@
 # CONSTRUCTOR METHOD => It initializes the attributes of an object. It is named 
 # as __init__.
 
+# DESTRUCTOR METHOD => It automatically deletes an object when it is not is used or had not 
+# dependencies. It is named as __del__.
+
+# DEL STATEMENT => It delete the object reference, then the Python garbage collector remove it from memory.
+
 # INSTANTIATE => Create an object.
 class Person:  # Class
-    def __init__ (self, name, phone_number, email, location):  # Constructor method
+    def __init__ (self, name, phone_number, email, location):  # Constructor method.
         self.name = name
         self.phone_number = phone_number  # Attribute.
         self.email = email
         self.location = location
     
-    def greeting(name):  # Method
-        print(f"Hello {name}.")
+    def greeting(self, name):  # Method.
+        print(f"Hello {name}. I'm {self.name}.")	
+        
+    def __del__(self):  # Destructor method.
+        print("The object has been deleted.")
 
 
 # Object  Class            Attribute
 #   ↓       ↓                  ↓
-User_1 = Person("mariana", 3142915739, "marian14@gmail.com", "tunja")
+user_1 = Person("mariana", 3142915739, "marian14@gmail.com", "tunja")
 #      ↑       ↑                                                    ↑
 #      │       ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-# Instantiate                    Constructor method
+# Instantiate                    Constructor method 
+
+user_1.greeting("Pablo")
+
+del user_1  # DeL statement.
