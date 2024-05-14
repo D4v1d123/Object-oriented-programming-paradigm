74a868fdfd6b6484a84e451ff527a72e21a8bcf0
David Guerrero <daviguer890@gmail.com>
2024-05-14 16:27:53 -0500

    Upload files deleted from the '06_method_overloading'folder in the remote repository.

--- 06_method_overloading/example/Main.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class Main {
-    public static void main(String[] args) {
-        AddNumbers.add(2.5, 4.8);  // Add decimal numbers.
-        AddNumbers.add(2, 4);  // Add integer numbers.
-    }
-}
