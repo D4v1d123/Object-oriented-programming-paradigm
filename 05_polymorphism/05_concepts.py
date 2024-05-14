a3776e70c60fbb79c7db111772ca0a0ad8d94c90
David Guerrero <daviguer890@gmail.com>
2024-05-13 17:37:52 -0700

    Set titles in capital case to improve readability.

--- 05_polymorphism/05_concepts.py
+++ 05_polymorphism/05_concepts.py
@@ -1,12 +1,12 @@
-# Polymorphism => It’s the ability of several classes to react to the same stimulus,
+# POLYMORPHISM => It’s the ability of several classes to react to the same stimulus,
 # this is done using the same method but with different behavior depending on the 
 # class.
 
-# Abstract class => There classes cannot be instantiated (they cannot create objects
+# ABSTRACT CLASS => There classes cannot be instantiated (they cannot create objects
 # directly), they can only be inherited, serving as a template for their child 
 # classes. An abstract class must have at least one abstract method.
 
-# Abstract method => It’s an empty method defined in the parent class (abstract 
+# ABSTRACT METHOD => It’s an empty method defined in the parent class (abstract 
 # class) that must necessarily be implemented in its child classes. In the parent
 # class it is defined what is going to be done (the action), but not how it is 
 # going to be done, and subsequently, in the child classes it’s established how the
